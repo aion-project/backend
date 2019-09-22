0c18f7d6a7bf2ee5ff448503a2cc58e79d0025b4
Udesh Kumarasinghe <mail@udesh.xyz>
2019-09-22 12:39:58 +0530

    Implemented cors configuration

--- src/main/kotlin/com/withaion/backend/security/SecurityConfig.kt
+++ src/main/kotlin/com/withaion/backend/security/SecurityConfig.kt
@@ -2,6 +2,7 @@ package com.withaion.backend.security
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity
 import org.springframework.security.config.web.server.ServerHttpSecurity
 import org.springframework.security.web.server.SecurityWebFilterChain
@@ -13,6 +14,7 @@ class SecurityConfig {
     internal fun springWebFilterChain(http: ServerHttpSecurity): SecurityWebFilterChain {
         http.csrf().disable()
                 .authorizeExchange()
+                .pathMatchers(HttpMethod.OPTIONS).permitAll()
                 .anyExchange().authenticated()
                 .and()
                 .oauth2ResourceServer()
