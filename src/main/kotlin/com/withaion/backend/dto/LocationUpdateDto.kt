8f9977501d9b8ef5d9eab9b32594ebacf4ac981e
Udesh Kumarasinghe <mail@udesh.xyz>
2019-10-11 09:36:20 +0530

    Add ac attribute to location model

--- src/main/kotlin/com/withaion/backend/dto/LocationUpdateDto.kt
+++ src/main/kotlin/com/withaion/backend/dto/LocationUpdateDto.kt
@@ -5,14 +5,16 @@ import com.withaion.backend.models.Location
 /**
  * DTO for location update requests
  *
- * @property name - Name of the new location
- * @property level - Level of the new location
- * @property description - Description of the new location
+ * @property name - Name of the location
+ * @property level - Level of the location
+ * @property description - Description of the location
+ * @property ac - Availability of air condition of the location
  * */
 data class LocationUpdateDto(
         val name: String?,
         val level: String?,
-        val description: String?
+        val description: String?,
+        val ac: Boolean?
 ) {
 
     fun toUpdatedLocation(location: Location): Location {
@@ -25,6 +27,9 @@ data class LocationUpdateDto(
         }
         description?.let {
             currentLocation = currentLocation.copy(description = description)
+        }
+        ac?.let {
+            currentLocation = currentLocation.copy(ac = ac)
         }
         return currentLocation
     }
