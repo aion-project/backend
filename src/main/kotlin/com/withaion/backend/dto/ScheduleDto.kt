390c12b353fdec1a17ce39df143d89d0a89e42e4
Udesh Kumarasinghe <mail@udesh.xyz>
2020-02-13 11:15:53 +0530

    Add until param to schedule new dto

--- src/main/kotlin/com/withaion/backend/dto/ScheduleDto.kt
+++ src/main/kotlin/com/withaion/backend/dto/ScheduleDto.kt
@@ -9,11 +9,12 @@ import java.time.LocalDateTime
 class ScheduleNewDto(
         val startDateTime: LocalDateTime,
         val endDateTime: LocalDateTime,
+        val until: LocalDateTime?,
         val repeat: RepeatType = RepeatType.NONE,
         val location: String,
         val event: String
 ) {
     fun toSchedule(location: Location, event: Event): Schedule {
-        return Schedule(startDateTime = startDateTime, endDateTime = endDateTime, repeatType = repeat, location = location, event = event)
+        return Schedule(startDateTime = startDateTime, endDateTime = endDateTime, until = until, repeatType = repeat, location = location, event = event)
     }
 }
